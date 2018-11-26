32b8aa80b0132ffd52f9411dac0be4d5d6192563
tueseed <tue_seed@hotmail.com>
2018-11-26 13:33:02 +0700

    Create connect-db.php

---
+++ db/connect-db.php
@@ -0,0 +1,8 @@
+<?php
+  server = "us-cdbr-iron-east-01.cleardb.net";
+	$username = "ba1c29a3a23619";
+	$password = "95eaae88";
+	$db = "heroku_324ce37d2926511";
+	$conn = new mysqli($server, $username, $password, $db);
+	mysqli_query($conn, "SET NAMES utf8");
+?>
