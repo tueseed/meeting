ea8c3f9204a35335047cc83c19c41f6322514381
tueseed <tue_seed@hotmail.com>
2018-11-26 13:47:29 +0700

    Add files via upload

--- db/connect-db.php
+++ db/connect-db.php
@@ -1,5 +1,5 @@
 <?php
-  server = "us-cdbr-iron-east-01.cleardb.net";
+	$server = "us-cdbr-iron-east-01.cleardb.net";
 	$username = "ba1c29a3a23619";
 	$password = "95eaae88";
 	$db = "heroku_324ce37d2926511";
