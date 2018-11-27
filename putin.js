25de1a8c1539612b06af26918fd325401e0de5f8
tueseed <tue_seed@hotmail.com>
2018-11-27 12:44:51 +0700

    Add files via upload

--- putin.js
+++ putin.js
@@ -5,12 +5,13 @@ function put_in(check,data)
 	var x = document.getElementById("check" + check).checked;
 	if(x == true)
 	{
-		document.getElementById("notice").value = document.getElementById("notice").value + "," + y;
+		document.getElementById("notice").value = document.getElementById("notice").value + "|" + y;
 	}
 	else if (x == false)
 	{
 		var str = document.getElementById("notice").value ;
 		str = str.replace(y,"");
+		document.getElementById("notice").value = str;
 	}
 	
 }
