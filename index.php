9c6642629386d54ed5396c7f939208fa85fa8e18
tueseed <tue_seed@hotmail.com>
2018-11-27 11:33:28 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -196,7 +196,7 @@
 												?>
 												<td>
 													<label class="switch">
-													  <input type="checkbox"  id="check<?php echo $obj["group_num"];?>" onchange="put_in(<?php echo $obj["group_name"];?>)">
+													  <input type="checkbox"  id="check<?php echo $obj["group_num"];?>" onchange="put_in(ภาษาไทย)">
 													  <span class="slider round"></span>
 													</label>
 												</td>
