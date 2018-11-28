7a9f336a990a9e2a7dec1fd5259c96e9e3a63e4d
tueseed <tue_seed@hotmail.com>
2018-11-28 12:47:16 +0700

    Add files via upload

--- testpush.php
+++ testpush.php
@@ -5,12 +5,12 @@
 		$query_meet = mysqli_query($conn,$sel_meet);
 		$obj = mysqli_fetch_array($query_meet);
 		$group_explode = explode("|",$obj["notice"]);
-		echo $obj["notice"];
-		/*foreach($group_explode as $group)
+		echo $obj["notice"]."<br>";
+		foreach($group_explode as $group)
 		{
-			$sel_group = "SELECT * FROM tbl_group WHERE group_name = ".$group;
-			$query_group = mysqli_query($conn,$sel_group);
-			$obj_group = mysqli_fetch_array($query_group);
-			push($obj["group_id"]);
-		}*/
+			//$sel_group = "SELECT * FROM tbl_group WHERE group_name = ".$group;
+			//$query_group = mysqli_query($conn,$sel_group);
+			//$obj_group = mysqli_fetch_array($query_group);
+			echo $group."<br>";
+		}
 ?>
