87deae6de27394836059dd78b171183f56454b01
tueseed <tue_seed@hotmail.com>
2018-11-28 12:23:25 +0700

    Add files via upload

---
+++ testpush.php
@@ -0,0 +1,16 @@
+<?php
+$access_token = '49sz8gFlKIm8wRU5fkeJF3VFZ0+RVe9vwv56SD68F0njV69PJCXRA3FyanXUDFx+THLRYmp3PscYBPBnTLdJyP/H61NkMX4VvcqBD3xageuhXzEq7QZyk02ie+QEvhdGtsDHNPLdV1XR2XnGO62U8gdB04t89/1O/w1cDnyilFU=';
+        $messages = [ 'type' => 'text','text' => "ทดสอบๆๆๆๆๆ"];
+        $url = 'https://api.line.me/v2/bot/message/push';
+        $data = ['to' => 'C6a54dc94c0f034766e58772ff9fd2994','messages' => [$messages]];
+        $post = json_encode($data);
+        $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        $result = curl_exec($ch);
+        curl_close($ch); 
+?>
