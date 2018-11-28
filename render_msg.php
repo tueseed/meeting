865db6d8973caca7d159d6725674f6adc7eefb28
tueseed <tue_seed@hotmail.com>
2018-11-28 20:35:33 +0700

    Add files via upload

--- render_msg.php
+++ render_msg.php
@@ -6,92 +6,152 @@ function flex_msg()
 				"type":"flex",
 				"altText":"PSQ V.2",
 				"contents":{
-								"type": "bubble",
-								"hero": {
-											"type": "image",
-											"url": "https://nutt-i.com/psqv2/PSQV2.png",
-											"size": "full",
-											"aspectRatio": "20:13",
-											"aspectMode": "cover",
-											"action": {
-														"type": "uri",
-														"uri": "https://nutt-i.com/psqv2/PSQV2.png"
-														}
-										},
-								"body": {
-											"type": "box",
-											"layout": "vertical",
-											"contents": [{
-															"type": "text",
-															"text": "ผลการค้นหา",
-															"weight": "bold",
-															"align":"center",
-															"size": "md"
-														},
-														{
-															"type": "text",
-															"text": "คำว่า ",
-															"weight": "bold",
-															"align":"center",
-															"size": "sm"
-														},
-														{
-															"type": "text",
-															"text": "1235132156",
-															"weight": "bold",
-															"align":"center",
-															"size": "sm"
-														},
-														{
-															"type": "separator",
-															"margin": "xxl"
-														}]
-										},
-								"footer": {
-											"type": "box",
-											"layout": "vertical",
-											"spacing": "sm",
-											"contents": [
-												{
-													"type": "button",
-													"style": "primary",
-													"height": "sm",
-													"action": {
-																"type": "uri",
-																"label": "56456456",
-																"uri": "https://nutt-i.com/psqv2"
-															}
-												},
-												{
-													"type": "button",
-													"style": "primary",
-													"height": "sm",
-													"action": {
-																"type": "uri",
-																"label": "45454545",
-																"uri": "https://nutt-i.com/psqv2"
-															}
-												},
-												{
-													"type": "button",
-													"style": "primary",
-													"height": "sm",
-													"action": {
-																"type": "uri",
-																"label": "ค้นหาแบบส่วนตัว(ทดลองใช้)",
-																"uri": "line://app/1544181630-9YeX5qxR"
-															}
-												},
-												{
-													"type": "spacer",
-													"size": "sm"
-												}
-												],
-									"flex": 0
-									}
-					
-				
-			}
+  "type": "bubble",
+  "header": {
+    "type": "box",
+    "layout": "horizontal",
+    "contents": [
+      {
+        "type": "text",
+        "text": "THE ASSISTANT",
+        "weight": "bold",
+        "color": "#5b2a72",
+        "align": "center",
+        "size": "md"
+      }
+    ]
+  },
+  "hero": {
+    "type": "image",
+    "url": "https://meetingnotify.herokuapp.com/pic/the-assist.png",
+    "size": "full",
+    "aspectRatio": "20:13",
+    "aspectMode": "cover"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "spacing": "sm",
+    "contents": [
+      {
+        "type": "text",
+        "text": "แจ้งเตือนการประชุม",
+        "size": "xl",
+         "align": "center",
+        "weight": "bold"
+      },
+      {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "box",
+            "layout": "baseline",
+            "contents": [
+              {
+                "type": "icon",
+                "url": "https://meetingnotify.herokuapp.com/pic/content.png"
+              },
+              {
+                "type": "text",
+                "text": "เรื่อง",
+                "weight": "bold",
+                "margin": "sm",
+                "flex": 0
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "baseline",
+            "contents": [
+              {
+                "type": "icon",
+                "url": "https://meetingnotify.herokuapp.com/pic/calendar.png"
+              },
+              {
+                "type": "text",
+                "text": "วันที่",
+                "weight": "bold",
+                "margin": "sm",
+                "flex": 0
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "baseline",
+            "contents": [
+              {
+                "type": "icon",
+                "url": "https://meetingnotify.herokuapp.com/pic/alarm-clock.png"
+              },
+              {
+                "type": "text",
+                "text":"เวลา",
+                "weight": "bold",
+                "margin": "sm",
+                "flex": 0
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "baseline",
+            "contents": [
+              {
+                "type": "icon",
+                "url": "https://meetingnotify.herokuapp.com/pic/meeting.png"
+              },
+              {
+                "type": "text",
+                "text": "ผู้เข้าประชุม",
+                "weight": "bold",
+                "margin": "sm",
+                "flex": 0
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "type": "text",
+        "text": "ผจก.พธร.,ffddlv,ds;vdv",
+        "wrap": true,
+        "color": "#aaaaaa",
+        "size": "xxs"
+      },
+      {
+        "type": "text",
+        "text": "รายละเอียด ดากสวฟ่าดสวดทาสฟทวดเห",
+        "wrap": true,
+        "color": "#aaaaaa",
+        "size": "xxs"
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "spacer",
+        "size": "xs"
+      },
+      {
+        "type": "button",
+        "style": "primary",
+        "color": "#5b2a72",
+        "action": {
+          "type": "uri",
+          "label": "เอกสารการประชุม",
+          "uri": "https://linecorp.com"
+        }
+      }
+    ]
+  }
+}
 	}';
 	$result = json_decode($json1);
 	return $result;
