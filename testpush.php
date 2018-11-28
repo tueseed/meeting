e10e394c5827648c9fc69d1add2caf8710aa0bec
tueseed <tue_seed@hotmail.com>
2018-11-28 14:15:18 +0700

    Add files via upload

--- testpush.php
+++ testpush.php
@@ -9,13 +9,11 @@
 			//$sel_group = "SELECT * FROM tbl_group WHERE group_name = '".$group_explode[1]."'";
 			//$query_group = mysqli_query($conn,$sel_group);
 			//$obj_group = mysqli_fetch_array($query_group);
-		
-		
-		
-		
-		
+		push();
+		function push()
+		{
 		$access_token = '49sz8gFlKIm8wRU5fkeJF3VFZ0+RVe9vwv56SD68F0njV69PJCXRA3FyanXUDFx+THLRYmp3PscYBPBnTLdJyP/H61NkMX4VvcqBD3xageuhXzEq7QZyk02ie+QEvhdGtsDHNPLdV1XR2XnGO62U8gdB04t89/1O/w1cDnyilFU=';
-        $messages = [ 'type' => 'text','text' => $obj["notice"]];
+        $messages = [ 'type' => 'text','text' => "jdakljkskljd"];
         $url = 'https://api.line.me/v2/bot/message/push';
         $data = ['to' => 'C6a54dc94c0f034766e58772ff9fd2994','messages' => [$messages]];
         $post = json_encode($data);
@@ -28,5 +26,5 @@
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         $result = curl_exec($ch);
         curl_close($ch); 
-		
+		}
 ?>
