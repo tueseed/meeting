0076c18102ed5456c3efbcaae130ba3e7b9a4959
tueseed <tue_seed@hotmail.com>
2018-11-28 14:07:11 +0700

    Add files via upload

--- testpush.php
+++ testpush.php
@@ -5,11 +5,11 @@
 		$query_meet = mysqli_query($conn,$sel_meet);
 		$obj = mysqli_fetch_array($query_meet);
 		$group_explode = explode("|",$obj["notice"]);
-		
-			$sel_group = "SELECT * FROM tbl_group WHERE group_name = '".$group_explode[1]."'";
-			$query_group = mysqli_query($conn,$sel_group);
-			$obj_group = mysqli_fetch_array($query_group);
-			push($obj_group["group_id"]);
+		$id ='C6a54dc94c0f034766e58772ff9fd2994';
+			//$sel_group = "SELECT * FROM tbl_group WHERE group_name = '".$group_explode[1]."'";
+			//$query_group = mysqli_query($conn,$sel_group);
+			//$obj_group = mysqli_fetch_array($query_group);
+			push($id);
 		
 		
 		
@@ -18,7 +18,7 @@
 		$access_token = '49sz8gFlKIm8wRU5fkeJF3VFZ0+RVe9vwv56SD68F0njV69PJCXRA3FyanXUDFx+THLRYmp3PscYBPBnTLdJyP/H61NkMX4VvcqBD3xageuhXzEq7QZyk02ie+QEvhdGtsDHNPLdV1XR2XnGO62U8gdB04t89/1O/w1cDnyilFU=';
         $messages = [ 'type' => 'text','text' => $obj["notice"]];
         $url = 'https://api.line.me/v2/bot/message/push';
-        $data = ['to' => $group_id,'messages' => [$messages]];
+        $data = ['to' => $id,'messages' => [$messages]];
         $post = json_encode($data);
         $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
         $ch = curl_init($url);
