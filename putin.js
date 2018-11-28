b96a37d3f70fb4652977ae924faab0574e4de9dd
tueseed <tue_seed@hotmail.com>
2018-11-28 12:39:16 +0700

    Add files via upload

--- putin.js
+++ putin.js
@@ -72,7 +72,7 @@ function push_now(meet_id)
 	var formData = new FormData();
 		formData.append('meet_id', meet_id);
 		$.ajax({
-			url: 'push_now.php',
+			url: 'testpush.php',
 			method: 'POST',
 			data: formData,
 			async: true,
