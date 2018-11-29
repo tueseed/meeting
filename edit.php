36396a6d3b14c35305cfc923600cb19b5ea65fc1
tueseed <tue_seed@hotmail.com>
2018-11-29 20:35:52 +0700

    Add files via upload

--- edit.php
+++ edit.php
@@ -144,6 +144,14 @@
 		</style>
 	</head>
 	<script>
+	function openNav() 
+	{
+		document.getElementById("mySidenav").style.width = "250px";
+	}
+
+	function closeNav() {
+		document.getElementById("mySidenav").style.width = "0";
+	}
 	</script>
 	<body>
 		<?php
