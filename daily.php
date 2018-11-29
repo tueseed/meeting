d8710134061de39dca3401c9d07814eefaee91d0
tueseed <tue_seed@hotmail.com>
2018-11-29 11:19:02 +0700

    Add files via upload

--- daily.php
+++ daily.php
@@ -1,13 +1,18 @@
 <?php
-	date_default_timezone_set("Asia/Bangkok");
-	$d1 = strtotime('21-12-2018');
-	$d2 = date('d-m-Y',$d1);
-	$today = date('d-m-Y');
-	$da = date_create($d2);
-	$db = date_create($today);
-	$num_day = date_diff($da,$db);
-	$nums = $num_day->days;
-	echo $nums;
-
+	require('db/connect-db.php');
+	$sql = "SELECT * FROM tbl_meeting";
+	$query = mysqli_query($conn,$sql);
+	while($ojb = mysqli_fetch_array($query))
+	{
+		date_default_timezone_set("Asia/Bangkok");
+		$d1 = strtotime($obj["meetinf_date"]);
+		$d2 = date('d-m-Y',$d1);
+		$today = date('d-m-Y');
+		$da = date_create($d2);
+		$db = date_create($today);
+		$num_day = date_diff($da,$db);
+		$nums = $num_day->days;
+		echo $nums;
+	}
 
 ?>
