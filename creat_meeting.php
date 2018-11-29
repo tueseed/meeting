2eedfa7b2b2c0522af12c7fee525e26094a106f9
tueseed <tue_seed@hotmail.com>
2018-11-29 10:01:55 +0700

    Add files via upload

--- creat_meeting.php
+++ creat_meeting.php
@@ -11,7 +11,9 @@
 	$meeting_detail = $_POST["meeting_detail"];
 	$notice = $_POST["notice"];
 	$link = $_POST["link"];
-	$sql_insert_meet = "INSERT INTO tbl_meeting(meeting_topic,meeting_date,meeting_time,meeting_time2,meeting_place,meeting_detail,notice,link) VALUES('$meeting_topic','$meeting_date','$meeting_time','$meeting_time2','$meeting_place','$meeting_detail','$notice','$link')";
+	$date1 = strtotime($meeting_date);
+	$date2 = date("d-m-Y",$date1);
+	$sql_insert_meet = "INSERT INTO tbl_meeting(meeting_topic,meeting_date,meeting_time,meeting_time2,meeting_place,meeting_detail,notice,link) VALUES('$meeting_topic','$date2','$meeting_time','$meeting_time2','$meeting_place','$meeting_detail','$notice','$link')";
 	mysqli_query($conn,$sql_insert_meet);
 	echo "สร้างการประชุมสำเร็จ";
 	}
