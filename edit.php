33ba526b6c2033faf372adf1b2c66e6007433c7c
tueseed <tue_seed@hotmail.com>
2018-11-29 22:47:03 +0700

    Add files via upload

--- edit.php
+++ edit.php
@@ -322,7 +322,7 @@
 										echo "<td>".$obj_meet["meeting_time2"]."</td>";
 										echo "<td>".$obj_meet["meeting_place"]."</td>";
 										echo "<td>".$obj_meet["meeting_detail"]."</td>";
-										$data = "'".$obj_meet["id"]."'".$obj_meet["meeting_topic"]."','".$obj_meet["meeting_date"]."','".$obj_meet["meeting_time"]."','".$obj_meet["meeting_time2"]."','".$obj_meet["meeting_place"]."','".$obj_meet["meeting_detail"]."','".$obj_meet["notice"]."','".$obj_meet["link"]."'";
+										$data = "'".$obj_meet["id"]."','".$obj_meet["meeting_topic"]."','".$obj_meet["meeting_date"]."','".$obj_meet["meeting_time"]."','".$obj_meet["meeting_time2"]."','".$obj_meet["meeting_place"]."','".$obj_meet["meeting_detail"]."','".$obj_meet["notice"]."','".$obj_meet["link"]."'";
 								?>
 										<td><input type="button" class="btn btn-success" value="แก้ไข" data-toggle="modal" href="#addtopic" onclick="send_edit(<?php echo $data;?>)"></td>
 										<td><input type="button" class="btn btn-success" value="ลอง" onclick="test(<?php echo "'".$obj_meet["notice"]."'";?>)"></td>
