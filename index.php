cae65a7e60dd2f7928e2a237b06d4318caef63fd
tueseed <tue_seed@hotmail.com>
2018-11-29 20:31:17 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -171,9 +171,8 @@
 		</div>
 		<div class="container-fluid" style="background-color:#b461fb;">
 			<div class="row row-center" >
-				<div class="col-lg-1" style="background-color:#9a9898;">
-					<h4><span style="cursor:pointer" onclick="openNav()">&#9776; MENU
-					</span></h4>
+				<div class="col-lg-1" >
+					<h3><span style="cursor:pointer" onclick="openNav()">&#9776;</span></h3>
 				</div>
 				<div class="col-lg-10" style="background-color:#b461fb;">
 					<h3>ระบบแจ้งเตือนการประชุมผ่านแอพพลิเคชั่นไลน์</h3>
