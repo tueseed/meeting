dae56589ac6ed9df4b55bd65883c2fe26f89aced
tueseed <tue_seed@hotmail.com>
2018-11-29 21:07:02 +0700

    Add files via upload

--- checknotice.php
+++ checknotice.php
@@ -1,6 +1,6 @@
 <?php
 	$notice = $_POST["notice"];
-	$group_explode = explode("|",$obj["notice"]);
+	$group_explode = explode("|",$notice);
 	foreach($group_explode as $group)
 		{
 			$group_return = $group_return."/".$group;
