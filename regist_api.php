fa49101c1f6b6276bbac54571dfe2003a150f435
tueseed <tue_seed@hotmail.com>
2018-11-30 21:10:57 +0700

    Add files via upload

--- regist_api.php
+++ regist_api.php
@@ -4,13 +4,7 @@
 	$name_lastname = $_POST["name_lastname"];
 	$pos = $_POST["pos"];
 	$office = $_POST["office"];
-	$sql_member = "INSERT INTO tbl_group(group_id,group_name,pos,office) VALUES('$uid','$name_lastname','$pos','$office');
+	$sql_member = "INSERT INTO tbl_group(group_id,group_name,pos,office) VALUES('$uid','$name_lastname','$pos','$office')";
 	$query_member = mysqli_query($conn,$sql_member);
-	$result = array();
-	while($res = mysqli_fetch_array($query_member))
-	{
-		array_push($result,$res);
-	}
-	
-	echo json_encode($result);
+	echo "เพิ่มข้อมูลสำเร็จ";
 ?>
