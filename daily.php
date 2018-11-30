34bce1735378f64ecfc1ac0359c1ed37c359b8e8
tueseed <tue_seed@hotmail.com>
2018-11-30 10:33:34 +0700

    Add files via upload

--- daily.php
+++ daily.php
@@ -1,5 +1,6 @@
 <?php
 	require('db/connect-db.php');
+	require('render_msg.php');
 	$sql = "SELECT * FROM tbl_meeting";
 	$query = mysqli_query($conn,$sql);
 	while($obj = mysqli_fetch_array($query))
@@ -12,7 +13,26 @@
 		$db = date_create($today);
 		$num_day = date_diff($da,$db);
 		$nums = $num_day->days;
-		echo $nums;
+		
+		echo $nums.$obj["notice"];
 	}
+	function push($id,$topic,$d,$t,$t1,$place,$detail)
+		{
+		$access_token = '49sz8gFlKIm8wRU5fkeJF3VFZ0+RVe9vwv56SD68F0njV69PJCXRA3FyanXUDFx+THLRYmp3PscYBPBnTLdJyP/H61NkMX4VvcqBD3xageuhXzEq7QZyk02ie+QEvhdGtsDHNPLdV1XR2XnGO62U8gdB04t89/1O/w1cDnyilFU=';
+        //$messages = [ 'type' => 'text','text' => "jdakljkskljd"];
+		$messages = flex_msg($topic,$d,$t,$t1,$place,$detail);
+        $url = 'https://api.line.me/v2/bot/message/push';
+        $data = ['to' => $id,'messages' => [$messages]];
+        $post = json_encode($data);
+        $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        $result = curl_exec($ch);
+        curl_close($ch); 
+		}
 
 ?>
